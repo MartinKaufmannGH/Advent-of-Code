8db5b9851fc07ef7e1fdefa4f628b8b3f064deb6
Martin Kaufmann <martin.betzkaufmann@gmail.com>
2024-12-02 10:45:55 +0100

    Horrible Solution for Day 2/1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+    /* DAY 1
     BufferedReader reader;
-    LinkedList<Integer> listOne = new LinkedList<>();
-    LinkedList<Integer> listTwo = new LinkedList<>();
-    File inp = new File("Day 1 Input");
     reader = new BufferedReader(new FileReader("Day 1 Input"));
     String line = reader.readLine();
+
+    LinkedList<Integer> listOne = new LinkedList<>();
+    LinkedList<Integer> listTwo = new LinkedList<>();
+
     while (line != null) {
       listOne.add(Integer.parseInt(line.split(" {3}")[0]));
       listTwo.add(Integer.parseInt(line.split(" {3}")[1]));
@@ -31,5 +36,40 @@ public class Main {
       result += current * num;
     }
     System.out.println(result);
+     */
+    BufferedReader reader;
+    reader = new BufferedReader(new FileReader("Day 2 Input"));
+    String line = reader.readLine();
+    int[] intArray;
+    int zaehler = 0;
+    Boolean safe = false;
+    while (line != null) {
+      intArray = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+      int[] sorted = intArray.clone();
+      Arrays.sort(sorted);
+      int[] sortedReverse = intArray.clone();
+      for (int i = 0; i < sorted.length; i++) {
+        sortedReverse[i] = sorted[sorted.length - 1 - i];
+      }
+      if (Arrays.equals(intArray, sortedReverse) || Arrays.equals(intArray, sorted)) {
+        for (int j = 0; j < intArray.length - 1; j++) {
+          safe = intArray[j] != intArray[j + 1] && Math.abs(intArray[j] - intArray[j + 1]) <= 3 ;
+          if(!safe) {
+            break;
+          }
+          System.out.println(intArray[j]);
+          System.out.println(intArray[j+1]);
+          System.out.println("-");
+        }
+        System.out.println(safe);
+        if (safe) {
+          zaehler++;
+          System.out.println("***");
+        }
+      }
+      line = reader.readLine();
+      safe = false;
+    }
+    System.out.println(zaehler);
   }
 }
